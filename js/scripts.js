eededb7f8a295466a07424c1403a577b48882bed
ryanfd <rdonnelly98@gmail.com>
2020-10-30 15:18:52 -0700

    fixed safari clip-paths

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
+// slide mobile nav back on item click
 $(document).ready(function(){
-    $("a.mobile").click(function(){ // slide mobile nav back on item click
-        $('input[type=checkbox]').click(); 
-
-      });
+  $("a.mobile").click(function(){
+    $('input[type=checkbox]').click(); 
+  });
 });
 
+
+// $(document).on('click', 'a[href^="#"]', function (e) { // hamburger transition on safari
+//   if (navigator.userAgent.search("Safari") >= 0) {
+//     $("input[type=checkbox]").click(function(){
+//       if($(this).prop("checked") == true) { // is checked
+//         $("#sidebarMenu").css({"-webkit-transform":"translateX(-250px)"});
+//       } else if ($(this).prop("checked") == false) { // unchecked
+//         $("#sidebarMenu").css({"-webkit-transform":"translateX(0px)"});
+//       }
+//     });
+//   }
+// });
